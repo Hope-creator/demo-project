530864a8074dd905c6cd1ce353becbd8810af21a
hope-creator <goryachkin.ivan1@mail.ru>
2020-11-10 22:24:56 +0400

    browserroute fix

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const AppContainer = connect(mapStateToProps, { initializeApp })(App);
 
 const MainApp = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
